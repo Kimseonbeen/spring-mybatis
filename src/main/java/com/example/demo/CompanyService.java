a440634b48088403a584d2074afd964c009d86fd
김선빈 <ksb9242@gmail.com>
2021-09-11 12:40:34 +0900

    spring mybatis

---
+++ src/main/java/com/example/demo/CompanyService.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CompanyService {
+
+    @Autowired
+    private CompanyMapper companyMapper;
+
+    @Autowired
+    private EmployeeMapper employeeMapper;
+
+    public List<Company> getAll() {
+        List<Company> companyList = companyMapper.getAll();
+
+        if(companyList != null && companyList.size() > 0) {
+            for (Company company : companyList) {
+                company.setEmployeeList(employeeMapper.getByCompanyId(company.getId()));
+            }
+        }
+
+        return companyList;
+
+    }
+
+    @Transactional
+    public Company add(Company company) {
+        companyMapper.insert(company);
+
+        //add company into legacy system
+        if (true) {
+            throw new RuntimeException("Legacy Exception");
+        }
+
+        return company;
+    }
+}
