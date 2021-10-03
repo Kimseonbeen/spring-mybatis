0d69fbb72a11cb25d3a9f024a5cd9928329f7883
김선빈 <ksb9242@gmail.com>
2021-10-03 23:16:40 +0900

    spring mybatis

---
+++ src/main/java/com/example/demo/CompanyMapper2.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface CompanyMapper2 {
+
+    @Insert("INSERT INTO company(company_name, company_address) VALUES(#{company.name}, #{company.address})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")   // return값이 0, 1이 아니라 자동생성된 id값을 같이 리턴해준다.
+    int insert(@Param("company") Company company);
+
+    @Select("SELECT * FROM company")
+    @Results(id = "CompanyMap", value = {           // id를 설정해주면 복붙 안 해도 재사용이 가능하다.
+            @Result(property = "name", column = "company_name"),
+            @Result(property = "address", column = "company_address"),
+            @Result(property = "employeeList", column = "id", many = @Many(select = "com.example.demo.EmployeeMapper.getByCompanyId"))
+    })
+    List<Company> getAll();
+
+    @Select("SELECT * FROM company WHERE id=#{id}")
+    @ResultMap("CompanyMap")
+    Company getById(@Param("id") int id);
+
+}
